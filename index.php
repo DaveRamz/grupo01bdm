7b729ccfb27e4c495ade08aba026107e6a4aa722
DaveRamz <david.rmz.95@gmail.com>
2022-03-29 21:17:08 -0600

    Cambios experimentales

--- index.php
+++ index.php
@@ -9,11 +9,9 @@
         <h1 class="estosSonLosChidos">Que onda!!</h1>
 
         <h1> Hola soy David </h1>
-
-        <h1 >Que onda?!?!?!?!</h1>
         
         <!-- inline --> 
-        <p style="color:green;">Hola, Estoy dentro del box model</p>
+        <p style="color:blue;">Hola, Estoy dentro del box model</p>
     </div>
     <div> 
         <p>Hola, Yo tambien estoy dentro del box model</p>
